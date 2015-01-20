70741c02df46f75bea4d23fd65d9a72e5a365c73
dshamis317 <dshamis317@gmail.com>
2015-01-19 20:40:56 -0500

    Add function to get search query

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,9 +10,20 @@ function stretchHeader() {
   $('.logo').bigtext();
 }
 
+function getQuery() {
+  $('.form').on('submit', function(e) {
+    e.preventDefault();
+    var $searchInput = $('.search-input');
+    var $query = $searchInput.val().toLowerCase();
+    $searchInput.val('');
+    var $searchDiv = $('.search-term');
+    $searchDiv.html($query);
+  })
+}
+
 RhymeSearch.initialize = function() {
   stretchHeader();
-
+  getQuery();
 }
 
 $(function() {
