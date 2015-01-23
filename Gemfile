cefed683087297cff142fde0164de3be49f3cbd3
dshamis317 <dshamis317@gmail.com>
2015-01-23 07:32:06 -0500

    Update gems and routes to render json

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'http://rubygems.org'
 
-gem 'sinatra'
+gem 'sinatra', git: 'git@github.com:sinatra/sinatra.git'
 gem 'sinatra-reloader'
 
 gem 'pry'
+gem 'pry-nav'
+
+gem 'httparty'
+gem 'json'
--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'bundler'
 Bundler.require
 
+api_key = ENV.fetch("WORDS_API")
+
 get '/' do
   File.read('index.html')
+end
+
+get '/:word' do
+  word = params[:word]
+  data = HTTParty.get(
+    "https://wordsapiv1.p.mashape.com/words/#{word}",
+    :headers => { "X-Mashape-Key" => "#{api_key}"}
+    )
+  data.to_json
 end
